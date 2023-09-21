2fde8c4a6f5319342cb35f94af4a53b94ff2cd48
Osorio-Tiago <68917717+Osorio-Tiago@users.noreply.github.com>
2023-09-20 22:05:55 -0600

    Actualizacion API

---
+++ Server/crud/fs-crud.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const readAboutUs = () => { 
+    const aboutUs = fs.readFileSync('./json-documents/about.json')
+    const aboutUsJson = JSON.parse(aboutUs)
+
+    return aboutUsJson
+}
+
+function save(req){
+    const { id, texto } = req.body;
+  
+    // Read data.json
+    let data = [];
+    try {
+      const jsonData = fs.readFileSync('./data.json', 'utf8');
+      data = JSON.parse(jsonData);
+    } catch (err) {
+      throw err
+    }
+    // Add new Object to data array
+    data.push({ id, texto });
+
+    // write data array to my data.json
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');
+}
+
+function read(req){
+    const { id } = req.params;
+    // Read data.json
+    try {
+      const jsonData = fs.readFileSync('data.json', 'utf8');
+      const data = JSON.parse(jsonData);
+  
+      // search object from my data by id
+      const script = data.find(item => item.id === id);
+
+    } catch (err) {
+      throw err
+    }
+
+    return script
+}
+
+module.exports = { readAboutUs, save, read }
--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const crud = require('./crud/fs-crud.js')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3001
 
 const server = app.listen(port, () => console.log('Server working on port ' + port))
 
+app.use(bodyParser.json());
+
+
 app.get("/api", (req, res) => {
     res.json({ "users" : ["Santiago", "Dylan", "Jose", "Paula" ]})
+})
+
+
+// GET METHOD RETURN ABOUT US INFO
+app.get('/', (req, res) => {
+    res.json(crud.readAboutUs())
 })
 
 
@@ -15,14 +26,32 @@ app.post('/compile', (req, res) =>{
     res.json({result : timestampedText}) // Returns json with timestamp + received text
 })
 
-//  POST METHOD
+//  POST METHOD WE DON'T KNOW WHAT THIS HAVE TO DO 
 app.post('/eval', (req, res) =>{
     // Don't know what to do here... yet
 })
 
 
-// GET AND SAVE SCRIPTS
+// LOAD AND SAVE SCRIPTS
+
+app.post('/script/save', (req, res) => {
+    try{
+        crud.save(req)
+    }catch(err){
+        res.status(500).json({ message: 'No se pudieron almacenar los datos' });
+    }
+    res.status(200).json({ message: 'Datos guardados correctamente' });
+  });
 
 app.get('/script/:id', (req, res) => {
-
+    try {
+        var script = crud.load(req)
+        if (script) {
+            res.status(200).json(script);
+          } else {
+            res.status(404).json({ message: 'No se encontró el script con el ID especificado' });
+          }
+    } catch (err) {
+        res.status(500).json({ message: 'Error al leer el archivo JSON' });
+    }
 })
