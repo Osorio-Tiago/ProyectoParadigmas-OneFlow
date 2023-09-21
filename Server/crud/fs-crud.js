047f2cc8d5864c976aae27fb831220ca9bf3f9a4
Osorio-Tiago <68917717+Osorio-Tiago@users.noreply.github.com>
2023-09-20 22:42:14 -0600

    Update fs-crud.js

--- Server/crud/fs-crud.js
+++ Server/crud/fs-crud.js
@@ -7,44 +7,40 @@ const readAboutUs = () => {
     return aboutUsJson
 }
 
-function save(req){
+const save = (req) => {
     const { id, texto } = req.body;
-
-    if(typeof id === 'undefined' || typeof texto === 'undefined'){
-        throw err
+  
+    if (typeof id === 'undefined' || typeof texto === 'undefined') {
+      throw err;
     }
-    // Read data.json
+  
     let data = [];
-    try {
+    try {    // Read data.json
       const jsonData = fs.readFileSync('./data.json', 'utf8');
       data = JSON.parse(jsonData);
-
-    // Add new Object to data array
-    data.push({ id, texto });
-
-    // write data array to my data.json
-    fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');
+  
+      data.push({ id, texto });
+  
+      fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');     // write data array to my data.json
     } catch (err) {
-      throw err
+      throw err;
     }
-}
+  };
 
-function read(req){
+
+  const read = (req) => {
     const { id } = req.params;
-    // Read data.json
-    try {
+      
+    try { // Read data.json
       const jsonData = fs.readFileSync('data.json', 'utf8');
       const data = JSON.parse(jsonData);
-      // search object from my data by id
-      const script = data.find(item => item.id === id);
-
-      return script
-
-    } catch (err) {
-      throw err
-    }
-
   
-}
+      const script = data.find((item) => item.id === id);     // search object from my data by id
+  
+      return script;
+    } catch (err) {
+      throw err;
+    }
+  };
 
 module.exports = { readAboutUs, save, read }
