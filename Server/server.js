20e6784aeaedbb50ee6d361aab68498d4e4c50d3
Osorio-Tiago <68917717+Osorio-Tiago@users.noreply.github.com>
2023-09-20 20:21:29 -0600

    Function compile complete
    save and load incomplete

--- Server/server.js
+++ Server/server.js
@@ -7,3 +7,22 @@ const server = app.listen(port, () => console.log('Server working on port ' + po
 app.get("/api", (req, res) => {
     res.json({ "users" : ["Santiago", "Dylan", "Jose", "Paula" ]})
 })
+
+
+//  POST METHOD RETURN SAME TEXT WITH TIMESTAMP ON THE BEGINNING
+app.post('/compile', (req, res) =>{
+    const timestampedText = `${new Date().toISOString()}: ${req.body.text}`; // Add timestamp to received text
+    res.json({result : timestampedText}) // Returns json with timestamp + received text
+})
+
+//  POST METHOD
+app.post('/eval', (req, res) =>{
+    // Don't know what to do here... yet
+})
+
+
+// GET AND SAVE SCRIPTS
+
+app.get('/script/:id', (req, res) => {
+
+})
