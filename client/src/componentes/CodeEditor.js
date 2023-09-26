d37aea865846ec61be988af95e9ff0f9529ef4fe
Jose0696 <jose96seque@gmail.com>
2023-09-26 12:47:02 -0600

    Update CodeEditor.js

--- client/src/componentes/CodeEditor.js
+++ client/src/componentes/CodeEditor.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import React, { useState, useRef, useEffect } from 'react';
 
 const keywords = [
   "HTML",
@@ -7,48 +6,63 @@ const keywords = [
   "Node.js",
   "php",
   "java"
-]
+];
 
 function CodeEditor() {
   const [code, setCode] = useState('');
-  const [output, setOutput] = useState(''); // Estado para almacenar la salida del código
-  const [matching_keywords, setMatching_keywords]= useState([]);
+  const [output, setOutput] = useState('');
+  const [matching_keywords, setMatching_keywords] = useState([]);
+  const textAreaRef = useRef(null);
+  const lineNumberRef = useRef(null);
 
-
-  function autoComplete(word){
-    const newCode = eliminarUltimaPalabra(code) + " " + word; 
+  function autoComplete(word) {
+    const newCode = eliminarUltimaPalabra(code) + " " + word;
     setCode(newCode);
   }
 
   const handleChange = ({ target: { value } }) => {
     setCode(value);
     const matches = keywords.filter((word) => word.includes(obtenerUltimaPalabra(value)));
-    //cadena.toLowerCase() implementar despues  
-    if (obtenerUltimaPalabra(value.trim()) == ""){
+
+    if (obtenerUltimaPalabra(value.trim()) === "") {
       setMatching_keywords([]);
     } else {
       setMatching_keywords(matches);
     }
   };
 
+  useEffect(() => {
+    // Actualizar la posición del contador de números al cambiar el contenido del textarea
+    lineNumberRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    lineNumberRef.current.scrollTop = textAreaRef.current.scrollTop;
+  }, [code]);
+
   function obtenerUltimaPalabra(texto) {
-    // Divide la cadena en palabras usando los espacios como separadores
     const palabras = texto.split(' ');
-    return palabras.length >=2 ? palabras[palabras.length - 1] : texto;    
+    return palabras.length >= 2 ? palabras[palabras.length - 1] : texto;
   }
- 
+
   const eliminarUltimaPalabra = (texto) => {
     const palabras = texto.split(' ');
     return palabras.length >= 2 ? palabras.slice(0, -1).join(' ') : '';
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      // Presionar Enter agrega una nueva línea, por lo que podemos contar las líneas en el código
+      const lineCount = code.split('\n').length;
+      // Actualizar el contador de números al agregar una nueva línea
+      lineNumberRef.current.style.height = `${lineCount * 20}px`;
+      lineNumberRef.current.scrollTop = textAreaRef.current.scrollTop = lineCount * 20;
+    }
+  };
+
   const runCode = () => {
     try {
-      // Utiliza una función anónima para evaluar el código y capturar la salida
       const result = Function('"use strict";return (' + code + ')')();
       setOutput(result);
     } catch (error) {
-      setOutput(`Error:${error.message}`);
+      setOutput(`Error: ${error.message}`);
     }
   };
 
@@ -57,56 +71,75 @@ function CodeEditor() {
     setOutput('');
   };
 
+  // Define el estilo para el textarea y el contador de números
+  const textareaStyle = {
+    width: '100%',
+    minWidth: '400px',
+    border: 'none',
+    outline: 'none',
+    resize: 'none',
+    padding: '10px',
+    boxSizing: 'border-box',
+    flexGrow: 1,
+    whiteSpace: 'pre-wrap',
+    overflowY: 'scroll',
+    maxHeight: '400px',
+    fontFamily: 'monospace', // Tipo de fuente para ambos elementos
+    fontSize: '14px', // Tamaño de letra para ambos elementos
+  };
+
   return (
     <>
-    <div style={{ display: 'flex' }}>
-      <div style={{ flex: '0 0 auto', padding: '10px', borderRight: '1px solid #ccc' }}>
-        {code.split('\n').map((line, index) => (
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: '0 0 auto', padding: '10px', borderRight: '1px solid #ccc' }}>
           <div
-            key={index}
+            ref={lineNumberRef}
             style={{
               textAlign: 'right',
               paddingRight: '5px',
-              color: '#fff', // Cambiar el color del texto a blanco
+              color: '#fff',
+              width: '2em',
+              whiteSpace: 'pre-wrap',
             }}
           >
-            {index + 1}
+            {code.split('\n').map((line, index) => (
+              <div key={index}>{index + 1}</div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div style={{ flex: '1', padding: '10px', overflow: 'auto' }}>
-        <textarea
-          value={code}
-          onChange={handleChange}
-          style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            outline: 'none',
-            resize: 'none',
-            padding: '10px', // Agregar relleno al área de texto
-            boxSizing: 'border-box', // Incluir el relleno en las dimensiones
-          }}
-        />
-        <button onClick={runCode}>Ejecutar</button>
-        <button onClick={clearCode}>Limpiar</button> {/* Botón para limpiar */}
-      </div>
-      <div style={{ flex: '1', padding: '10px' }}>
-        <div>
-          <h2>Resultado:</h2>
-          <pre>{output}</pre>
         </div>
-        <div>
-        {matching_keywords.map((word) => (
-        <button onClick={()=>{autoComplete(word)} } key={word}>{word}</button>
-       ))}
-</div>
+        <div style={{ flex: '1', padding: '10px', display: 'flex', flexDirection: 'column' }}>
+          <textarea
+            ref={textAreaRef}
+            value={code}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            style={textareaStyle}
+          />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-start',
+              marginTop: '10px',
+            }}
+          >
+            <button onClick={runCode}>Ejecutar</button>
+            <button onClick={clearCode}>Limpiar</button>
+          </div>
+        </div>
+        <div style={{ flex: '1', padding: '10px' }}>
+          <div>
+            <h2>Resultado:</h2>
+            <pre style={{ height: '400px' }}>{output}</pre>
+          </div>
+          <div>
+            {matching_keywords.map((word) => (
+              <button onClick={() => { autoComplete(word) }} key={word}>{word}</button>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
 
 export default CodeEditor;
-
-
