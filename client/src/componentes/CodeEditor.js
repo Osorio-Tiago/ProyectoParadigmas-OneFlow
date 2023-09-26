cba463436bc9b74c7c61a00db176e0c5c62bf094
Jose0696 <jose96seque@gmail.com>
2023-09-25 20:44:31 -0600

    Update CodeEditor.js

--- client/src/componentes/CodeEditor.js
+++ client/src/componentes/CodeEditor.js
@@ -2,28 +2,59 @@ import React, { useState } from 'react';
 
 function CodeEditor() {
   const [code, setCode] = useState('');
+  const [output, setOutput] = useState(''); // Estado para almacenar la salida del código
 
   const handleChange = (e) => {
     setCode(e.target.value);
   };
 
-  const codeLines = code.split('\n');
+  const runCode = () => {
+    try {
+      // Utiliza una función anónima para evaluar el código y capturar la salida
+      const result = Function('"use strict";return (' + code + ')')();
+      setOutput(result);
+    } catch (error) {
+      setOutput(`Error: ${error.message}`);
+    }
+  };
 
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ flex: '0 0 auto', padding: '10px', borderRight: '1px solid #ccc' }}>
-        {codeLines.map((line, index) => (
-          <div key={index} style={{ textAlign: 'right', paddingRight: '5px', color: '#999' }}>
+        {code.split('\n').map((line, index) => (
+          <div
+            key={index}
+            style={{
+              textAlign: 'right',
+              paddingRight: '5px',
+              color: '#fff', // Cambiar el color del texto a blanco
+            }}
+          >
             {index + 1}
           </div>
         ))}
       </div>
-      <div style={{ flex: '1', padding: '10px' }}>
+      <div style={{ flex: '1', padding: '10px', overflow: 'auto' }}>
         <textarea
           value={code}
           onChange={handleChange}
-          style={{ width: '100%', height: '100%', border: 'none', outline: 'none', resize: 'none' }}
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',
+            outline: 'none',
+            resize: 'none',
+            padding: '10px', // Agregar relleno al área de texto
+            boxSizing: 'border-box', // Incluir el relleno en las dimensiones
+          }}
         />
+        <button onClick={runCode}>Ejecutar</button>
+      </div>
+      <div style={{ flex: '1', padding: '10px' }}>
+        <div>
+          <h2>Resultado:</h2>
+          <pre>{output}</pre>
+        </div>
       </div>
     </div>
   );
