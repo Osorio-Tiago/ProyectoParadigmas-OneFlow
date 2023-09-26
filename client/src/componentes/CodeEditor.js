d10f0da1c57e1bbf7a80c36ac115eb7878744b79
dylancr27 <damolina27@hotmail.com>
2023-09-26 16:16:05 -0600

    modificacion vista

--- client/src/componentes/CodeEditor.js
+++ client/src/componentes/CodeEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const keywords = [
   "HTML",
@@ -12,30 +12,22 @@ function CodeEditor() {
   const [code, setCode] = useState('');
   const [output, setOutput] = useState('');
   const [matching_keywords, setMatching_keywords] = useState([]);
-  const textAreaRef = useRef(null);
-  const lineNumberRef = useRef(null);
 
   function autoComplete(word) {
     const newCode = eliminarUltimaPalabra(code) + " " + word;
     setCode(newCode);
+    setMatching_keywords([]);
   }
 
   const handleChange = ({ target: { value } }) => {
     setCode(value);
     const matches = keywords.filter((word) => word.includes(obtenerUltimaPalabra(value)));
-
     if (obtenerUltimaPalabra(value.trim()) === "") {
       setMatching_keywords([]);
     } else {
       setMatching_keywords(matches);
     }
   };
-
-  useEffect(() => {
-    // Actualizar la posición del contador de números al cambiar el contenido del textarea
-    lineNumberRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
-    lineNumberRef.current.scrollTop = textAreaRef.current.scrollTop;
-  }, [code]);
 
   function obtenerUltimaPalabra(texto) {
     const palabras = texto.split(' ');
@@ -45,16 +37,6 @@ function CodeEditor() {
   const eliminarUltimaPalabra = (texto) => {
     const palabras = texto.split(' ');
     return palabras.length >= 2 ? palabras.slice(0, -1).join(' ') : '';
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      // Presionar Enter agrega una nueva línea, por lo que podemos contar las líneas en el código
-      const lineCount = code.split('\n').length;
-      // Actualizar el contador de números al agregar una nueva línea
-      lineNumberRef.current.style.height = `${lineCount * 20}px`;
-      lineNumberRef.current.scrollTop = textAreaRef.current.scrollTop = lineCount * 20;
-    }
   };
 
   const runCode = () => {
@@ -71,65 +53,58 @@ function CodeEditor() {
     setOutput('');
   };
 
-  // Define el estilo para el textarea y el contador de números
-  const textareaStyle = {
-    width: '100%',
-    minWidth: '400px',
-    border: 'none',
-    outline: 'none',
-    resize: 'none',
-    padding: '10px',
-    boxSizing: 'border-box',
-    flexGrow: 1,
-    whiteSpace: 'pre-wrap',
-    overflowY: 'scroll',
-    maxHeight: '400px',
-    fontFamily: 'monospace', // Tipo de fuente para ambos elementos
-    fontSize: '14px', // Tamaño de letra para ambos elementos
-  };
-
   return (
     <>
       <div style={{ display: 'flex' }}>
         <div style={{ flex: '0 0 auto', padding: '10px', borderRight: '1px solid #ccc' }}>
-          <div
-            ref={lineNumberRef}
-            style={{
-              textAlign: 'right',
-              paddingRight: '5px',
-              color: '#fff',
-              width: '2em',
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {code.split('\n').map((line, index) => (
-              <div key={index}>{index + 1}</div>
-            ))}
-          </div>
+          {code.split('\n').map((line, index) => (
+            <div
+              key={index}
+              style={{
+                textAlign: 'right',
+                paddingRight: '5px',
+                color: '#fff',
+              }}
+            >
+              {index + 1}
+            </div>
+          ))}
         </div>
-        <div style={{ flex: '1', padding: '10px', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ flex: '1', padding: '10px', overflow: 'auto' }}>
           <textarea
-            ref={textAreaRef}
             value={code}
             onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            style={textareaStyle}
-          />
-          <div
             style={{
-              display: 'flex',
-              justifyContent: 'flex-start',
-              marginTop: '10px',
+              width: '100%',
+              height: '400px', // Ajusta la altura como desees
+              border: 'none',
+              outline: 'none',
+              resize: 'none',
+              padding: '10px',
+              boxSizing: 'border-box',
+              whiteSpace: 'nowrap', // Evita el ajuste automático de líneas
+              overflowX: 'auto', // Agrega barra de desplazamiento horizontal si es necesario
             }}
-          >
-            <button onClick={runCode}>Ejecutar</button>
-            <button onClick={clearCode}>Limpiar</button>
-          </div>
+          />
+          <button onClick={runCode}>Ejecutar</button>
+          <button onClick={clearCode}>Limpiar</button>
         </div>
         <div style={{ flex: '1', padding: '10px' }}>
           <div>
-            <h2>Resultado:</h2>
-            <pre style={{ height: '400px' }}>{output}</pre>
+            <textarea
+              value={output}
+              readOnly
+              style={{
+                width: '100%',
+                height: '400px', // Ajusta la altura como desees
+                border: 'none',
+                outline: 'none',
+                resize: 'none',
+                padding: '10px',
+                boxSizing: 'border-box',
+                overflowX: 'scroll', // Agrega barra de desplazamiento horizontal si es necesario
+              }}
+            />
           </div>
           <div>
             {matching_keywords.map((word) => (
@@ -143,3 +118,7 @@ function CodeEditor() {
 }
 
 export default CodeEditor;
+
+
+
+
